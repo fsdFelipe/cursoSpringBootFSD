6995c53ad6a7fbed15ef37fc1599b39fb2c76c2a
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-20 11:46:58 -0300

    Projeto Criado

---
+++ src/main/java/com/app/CursoSpringBootFsdApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootFsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootFsdApplication.class, args);
+	}
+
+}
