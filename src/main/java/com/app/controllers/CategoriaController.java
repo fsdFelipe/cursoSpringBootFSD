b6b6a9eb5f62e544600a27ea1b48092057eb4127
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-21 20:50:18 -0300

    Criando Repository Service e método buscar categoria por id

--- src/main/java/com/app/controllers/CategoriaController.java
+++ src/main/java/com/app/controllers/CategoriaController.java
@@ -1,16 +1,30 @@
 package com.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.domain.Categoria;
+import com.app.services.CategoriaService;
 
 @RestController
 @RequestMapping("/")
 public class CategoriaController {
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	@GetMapping("/categorias")
 	public String bemVindo() {
 		return "Controlador REST OK !";
+	}
+	
+	public ResponseEntity<?> buscar(@PathVariable Integer id){
+		Categoria obj = categoriaService.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
