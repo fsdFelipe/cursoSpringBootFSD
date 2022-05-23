bf6aafdca7acd8d836677eef4d38682018d6d3e0
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-23 20:04:19 -0300

    Listando todas Categorias - Usando DTO

--- src/main/java/com/app/controllers/CategoriaController.java
+++ src/main/java/com/app/controllers/CategoriaController.java
@@ -1,6 +1,8 @@
 package com.app.controllers;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.domain.Categoria;
+import com.app.domain.dto.CategoriaDTO;
 import com.app.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,13 @@ public class CategoriaController {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> Listar(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
