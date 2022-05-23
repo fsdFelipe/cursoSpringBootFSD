55ab288979fa65d81e0e5d89b40a04be8cf0dcdc
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-23 19:56:29 -0300

    Inserir, Alterar e Deletar Categorias

--- src/main/java/com/app/controllers/CategoriaController.java
+++ src/main/java/com/app/controllers/CategoriaController.java
@@ -1,31 +1,54 @@
 package com.app.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.domain.Categoria;
 import com.app.services.CategoriaService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/categorias")
 public class CategoriaController {
 	
 	@Autowired
-	private CategoriaService categoriaService;
+	private CategoriaService service;
 	
-	@GetMapping("/categorias")
-	public String bemVindo() {
-		return "Controlador REST OK !";
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
-	@GetMapping("/categorias/{id}")
-	public ResponseEntity<?> buscar(@PathVariable Integer id){
-		Categoria obj = categoriaService.buscar(id);
-		return ResponseEntity.ok().body(obj);
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/app/services/CategoriaService.java
+++ src/main/java/com/app/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.app.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.app.domain.Categoria;
 import com.app.repositories.CategoriaRepository;
+import com.app.services.exceptions.DataIntegrityException;
 import com.app.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +21,24 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objet não encontrado ! id:" +id+
 				"Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui produtos");
+		}
 	}
 
 }
