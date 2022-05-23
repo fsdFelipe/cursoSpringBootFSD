33bf8b35c2ca3fa0fd083869221583e45c3cd321
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-23 18:52:11 -0300

    Associação muitos para muitos (Categoria - Produtos)

--- src/main/java/com/app/services/DBService.java
+++ src/main/java/com/app/services/DBService.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.domain.Categoria;
+import com.app.domain.Produto;
 import com.app.repositories.CategoriaRepository;
+import com.app.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public void instantiateTestDataBase() {
 		
@@ -25,7 +29,33 @@ public class DBService {
 		Categoria cat4 = new Categoria();
 		cat4.setNome("Sucos");
 		
+		Produto prod1 = new Produto();
+		prod1.setNome("XBurguer");
+		prod1.setPreco(10.00);
+		Produto prod2 = new Produto();
+		prod2.setNome("XFrango");
+		prod2.setPreco(15.00);
+		Produto prod3= new Produto();
+		prod3.setNome("coxinha");
+		prod3.setPreco(3.70);
+		Produto prod4 = new Produto();
+		prod4.setNome("Laranja");
+		prod4.setPreco(1.00);
+		
+		cat2.getProdutos().addAll(Arrays.asList(prod1,prod2));
+		cat3.getProdutos().addAll(Arrays.asList(prod3));
+		cat4.getProdutos().addAll(Arrays.asList(prod4));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat2));
+		prod2.getCategorias().addAll(Arrays.asList(cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat3));
+		prod4.getCategorias().addAll(Arrays.asList(cat4));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3,prod4));
+		
 	}
+	
+
 
 }
