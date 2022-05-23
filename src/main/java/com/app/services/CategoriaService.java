bbdb9ae235f1c83a2cb6fc512e962282b2b31942
fsdFelipe <devfsdfelipe@gmail.com>
2022-05-23 19:30:08 -0300

    ObjectNotFoundException, StandardError, ResourceExceptionHandler

--- src/main/java/com/app/services/CategoriaService.java
+++ src/main/java/com/app/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.app.domain.Categoria;
 import com.app.repositories.CategoriaRepository;
+import com.app.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objet não encontrado ! id:" +id+
+				"Tipo: " + Categoria.class.getName()));
 	}
 
 }
